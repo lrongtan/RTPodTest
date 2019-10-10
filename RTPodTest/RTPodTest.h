6bab661d0e1412da8a24a4e856fad7eff5aae1c7
LiRongTan <844928336@qq.com>
2019-10-10 15:06:56 +0800

    创建

---
+++ RTPodTest/RTPodTest.h
@@ -0,0 +1,19 @@
+//
+//  RTPodTest.h
+//  RTPodTest
+//
+//  Created by 李荣潭 on 2019/10/10.
+//  Copyright © 2019 Lezhizhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RTPodTest.
+FOUNDATION_EXPORT double RTPodTestVersionNumber;
+
+//! Project version string for RTPodTest.
+FOUNDATION_EXPORT const unsigned char RTPodTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RTPodTest/PublicHeader.h>
+
+
